e1172d85276f8fcb2eafe8f0bf4140387ff5ac1d
Αlex Ηattie <akapellas@outlook.com>
2025-09-05 13:39:11 +0300

    fundamentals: vars + outputs demo

--- main.tf
+++ main.tf
@@ -9,11 +9,21 @@ terraform {
 
 provider "random" {}
 
+variable "prefix" {
+  type        = string
+  description = "Προαιρετικό prefix για το pet name"
+  default     = ""
+}
+
+locals {
+  computed_prefix = var.prefix != "" ? "${var.prefix}-" : ""
+}
+
 resource "random_pet" "demo" {
   length    = 2
   separator = "-"
 }
 
 output "pet_name" {
-  value = random_pet.demo.id
+  value = "${local.computed_prefix}${random_pet.demo.id}"
 }
