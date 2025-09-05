757f8bde2d39e0472e592af8d75719bca8d9b69e
Αlex Ηattie <akapellas@outlook.com>
2025-09-06 00:09:19 +0300

    add module

--- main.tf
+++ main.tf
@@ -1,6 +1,21 @@
-variable "prefix"       { type = string  default = "" }
-variable "length"       { type = number  default = 2 }
-variable "token_length" { type = number  default = 16  sensitive = true }
+variable "prefix" {
+  type        = string
+  default     = ""
+  description = "Prefix for the pet name"
+}
+
+variable "length" {
+  type        = number
+  default     = 2
+  description = "Word count for random_pet"
+}
+
+variable "token_length" {
+  type        = number
+  default     = 16
+  sensitive   = true
+  description = "Length of demo access token"
+}
 
 module "pet" {
   source       = "./modules/pet"
@@ -9,6 +24,15 @@ module "pet" {
   token_length = var.token_length
 }
 
-output "pet_name"   { value = module.pet.pet_name }
-output "access_token" { value = module.pet.access_token  sensitive = true }
-output "workspace"  { value = terraform.workspace }
+output "pet_name" {
+  value = module.pet.pet_name
+}
+
+output "access_token" {
+  value     = module.pet.access_token
+  sensitive = true
+}
+
+output "workspace" {
+  value = terraform.workspace
+}
