e560b1d506d73ccf4d5b08d5da2466f825de233a
Αlex Ηattie <akapellas@outlook.com>
2025-09-05 23:50:35 +0300

    add module

--- modules/pet/outputs.tf
+++ modules/pet/outputs.tf
@@ -1,2 +1,8 @@
-output "pet_name"     { value = "${local.computed_prefix}${random_pet.demo.id}" }
-output "access_token" { value = random_password.token.result  sensitive = true }
+output "pet_name" {
+  value = "${local.computed_prefix}${random_pet.demo.id}"
+}
+
+output "access_token" {
+  value     = random_password.token.result
+  sensitive = true
+}
--- modules/pet/variables.tf
+++ modules/pet/variables.tf
@@ -1,3 +1,18 @@
-variable "prefix"        { type = string  default = "" }
-variable "length"        { type = number  default = 2 }
-variable "token_length"  { type = number  default = 16  sensitive = true }
+variable "prefix" {
+  type        = string
+  default     = ""
+  description = "Prefix for the pet name"
+}
+
+variable "length" {
+  type        = number
+  default     = 2
+  description = "Word count for random_pet"
+}
+
+variable "token_length" {
+  type        = number
+  default     = 16
+  sensitive   = true
+  description = "Length of demo access token"
+}
